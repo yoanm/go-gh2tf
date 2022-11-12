0b37d6693a7423ff98325b1144d459b9cc2bd3da
Yoanm <yoanm@users.noreply.github.com>
2022-11-12 09:16:34 +0100

    improve

--- value_generator.go
+++ value_generator.go
@@ -2,7 +2,9 @@ package gh2tf
 
 import "github.com/yoanm/go-tfsig"
 
-// NewValueGenerator returns a new ValueGenerator with all terraform gitHub resources escaped to consider as 'ident' tokens
+// NewValueGenerator returns a new ValueGenerator with provided list of prefix to consider as 'ident' tokens
+//
+// Default `tfsig` ident tokens and terraform gitHub resources will be automatically added
 func NewValueGenerator(identPrefixList ...string) tfsig.ValueGenerator {
 	return tfsig.NewValueGeneratorWith(NewIdentTokenMatcher(identPrefixList...))
 }
